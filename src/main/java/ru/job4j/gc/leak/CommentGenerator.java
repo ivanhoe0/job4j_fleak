1f472ba43bb347bc92d6790dc359683f0cf64f29
ivanhoe <corsair954@mail.ru>
2025-06-23 18:56:43 +0300

    4. Найти утечку памяти [#504882]

--- src/main/java/ru/job4j/gc/leak/CommentGenerator.java
+++ src/main/java/ru/job4j/gc/leak/CommentGenerator.java
@@ -8,10 +8,10 @@ import java.util.Random;
 
 public class CommentGenerator implements Generate {
 
-    public final String PATH_PHRASES = "files/phrases.txt";
+    public final String pathPhrases = "files/phrases.txt";
 
-    public final String SEPARATOR = System.lineSeparator();
-    private final List<Comment> COMMENTS = new ArrayList<>();
+    public final String separator = System.lineSeparator();
+    private final List<Comment> comments = new ArrayList<>();
     public static final Integer COUNT = 50;
     private List<String> phrases;
     private final UserGenerator userGenerator;
@@ -25,28 +25,28 @@ public class CommentGenerator implements Generate {
 
     private void read() {
         try {
-            phrases = read(PATH_PHRASES);
+            phrases = read(pathPhrases);
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
     }
 
     public List<Comment> getComments() {
-        return COMMENTS;
+        return comments;
     }
 
     @Override
     public void generate() {
-        COMMENTS.clear();
+        comments.clear();
         for (int i = 0; i < COUNT; i++) {
             String text = String.format("%s%s%s%s%s",
-                    phrases.get(random.nextInt(phrases.size())), SEPARATOR,
-                    phrases.get(random.nextInt(phrases.size())), SEPARATOR,
+                    phrases.get(random.nextInt(phrases.size())), separator,
+                    phrases.get(random.nextInt(phrases.size())), separator,
                     phrases.get(random.nextInt(phrases.size())));
             var comment = new Comment();
             comment.setText(text);
             comment.setUser(userGenerator.randomUser());
-            COMMENTS.add(comment);
+            comments.add(comment);
         }
     }
 }
