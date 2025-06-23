569503e5bec662da0f32e2c62108b046de4f3e44
ivanhoe <corsair954@mail.ru>
2025-06-23 18:46:15 +0300

    4. Найти утечку памяти [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/CommentGenerator.java
@@ -0,0 +1,52 @@
+package ru.job4j.gc.leak;
+
+import ru.job4j.gc.leak.models.Comment;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CommentGenerator implements Generate {
+
+    public final String PATH_PHRASES = "files/phrases.txt";
+
+    public final String SEPARATOR = System.lineSeparator();
+    private final List<Comment> COMMENTS = new ArrayList<>();
+    public static final Integer COUNT = 50;
+    private List<String> phrases;
+    private final UserGenerator userGenerator;
+    private final Random random;
+
+    public CommentGenerator(Random random, UserGenerator userGenerator) {
+        this.userGenerator = userGenerator;
+        this.random = random;
+        read();
+    }
+
+    private void read() {
+        try {
+            phrases = read(PATH_PHRASES);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public List<Comment> getComments() {
+        return COMMENTS;
+    }
+
+    @Override
+    public void generate() {
+        COMMENTS.clear();
+        for (int i = 0; i < COUNT; i++) {
+            String text = String.format("%s%s%s%s%s",
+                    phrases.get(random.nextInt(phrases.size())), SEPARATOR,
+                    phrases.get(random.nextInt(phrases.size())), SEPARATOR,
+                    phrases.get(random.nextInt(phrases.size())));
+            var comment = new Comment();
+            comment.setText(text);
+            comment.setUser(userGenerator.randomUser());
+            COMMENTS.add(comment);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/gc/leak/UserGenerator.java
@@ -0,0 +1,56 @@
+package ru.job4j.gc.leak;
+
+import ru.job4j.gc.leak.models.User;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class UserGenerator implements Generate {
+
+    public final String PATH_NAMES = "files/names.txt";
+    public final String PATH_SURNAMES = "files/surnames.txt";
+    public final String PATH_PATRONS = "files/patr.txt";
+
+    public final String SEPARATOR = " ";
+    public final Integer NEW_USERS = 1000;
+
+    public List<String> names;
+    public List<String> surnames;
+    public List<String> patrons;
+    private final List<User> USERS = new ArrayList<>();
+    private final Random random;
+
+    public UserGenerator(Random random) {
+        this.random = random;
+        readAll();
+    }
+
+    @Override
+    public void generate() {
+        USERS.clear();
+        for (int i = 0; i < NEW_USERS; i++) {
+            var name = surnames.get(random.nextInt(surnames.size())) + SEPARATOR
+                    + names.get(random.nextInt(names.size())) + SEPARATOR
+                    + patrons.get(random.nextInt(patrons.size()));
+            var user = new User();
+            user.setName(name);
+            USERS.add(user);
+        }
+    }
+
+    private void readAll() {
+        try {
+            names = read(PATH_NAMES);
+            surnames = read(PATH_SURNAMES);
+            patrons = read(PATH_PATRONS);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public User randomUser() {
+        return USERS.get(random.nextInt(USERS.size()));
+    }
+}
